971b1e8523ded77f544908569437347e0f889f26
Kairui Song <kasong@redhat.com>
2016-06-21 14:30:54 +0800

    Remove update check related features

--- setup_db.py
+++ setup_db.py
@@ -13,7 +13,6 @@ from django.db import transaction
 
 from caselink.tasks import update_polarion
 from caselink.tasks import update_linkage
-from caselink.tasks import update_changes
 
 def run():
     print('Loading polaroin')
@@ -22,8 +21,6 @@ def run():
     print('Loading linkage')
     update_linkage()
 
-    print('Loading changes')
-    update_changes()
 
 if __name__ == '__main__':
     reload(sys)
