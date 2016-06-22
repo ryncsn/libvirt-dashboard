bf9fbf2978ed494c681efa4d8b41f915ba107867
Kairui Song <kasong@redhat.com>
2016-06-22 13:51:12 +0800

    Clean up and restruct job.py.
    Remove 'change' attribute related functions, no need to load old baseline.
    
    Now Job.py only fetch polarion cases, and merge with caselink
    file to generate a baseline yaml file.

--- caselink/job.py
+++ caselink/job.py
@@ -21,6 +21,48 @@ from pylarion.work_item import _WorkItem
 from pylarion.wiki_page import WikiPage
 
 
+PROJECT = 'RedHatEnterpriseLinux7'
+AUTO_SPACE = 'Virt-LibvirtAuto'
+MANUAL_SPACE = 'Virt-LibvirtQE'
+
+
+ALL_CASE_DEFAULT_ATTR = {
+    'polarion': '',
+    'title': '',
+    'tcms': '',
+    'feature': '',
+    'comment': '',
+    'automated': False,
+    'el6': True,
+    'el7': True,
+    'type': '',
+    'updated': None,
+    'created': False,
+    'removed': False,
+    'project': PROJECT,
+    'component': 'libvirt',
+    'cases': [],
+    'documents': [],
+    'errors': [],
+    'changes': [],
+    'diffs': [],
+}
+
+
+BASELINE_WORKITEM_ATTR = {
+    'title': '',
+    'type': '',
+    'automated': '',
+    'commit': '',
+    'arch': '',
+    'documents': '',
+    'component': 'libvirt',
+    'project': PROJECT,
+    'updated': '',
+    'errors': ['Empty error'],
+}
+
+
 def exc_hook(exctype, value, trace):
     """
     Customized exception hook function to enable pdb when unhandled
@@ -41,23 +83,7 @@ def exc_hook(exctype, value, trace):
 
 # Override exception hook to jump into pdb when exception
 sys.excepthook = exc_hook
-
-PROJECT = 'RedHatEnterpriseLinux7'
-
-AUTO_SPACE = 'Virt-LibvirtAuto'
-MANUAL_SPACE = 'Virt-LibvirtQE'
 service = _WorkItem.session.tracker_client.service
-
-
-def info(text, *args):
-    print(text % args)
-
-
-def wiki_escape(text):
-    text = text.replace('[', '\\[')
-    text = text.replace(']', '\\]')
-    text = text.replace('-', '\\-')
-    return text
 
 
 class literal(unicode):
@@ -103,6 +129,19 @@ def suds_2_object(suds_obj):
     return obj
 
 
+def flatten_cases(cases):
+    all_cases = {}
+    for doc_id, doc in cases['documents'].items():
+        wis = doc['work_items']
+        for wi_id, wi in wis.items():
+            if 'documents' in wi:
+                wi['documents'].append(doc_id)
+            else:
+                wi['documents'] = [doc_id]
+            all_cases[wi_id] = wi
+    return all_cases
+
+
 def load_polarion(project, space):
     obj = OrderedDict([
         ('project', literal(project)),
@@ -112,7 +151,7 @@ def load_polarion(project, space):
 
     docs = Document.get_documents(
         project, space, fields=['document_id', 'title', 'type', 'updated', 'project_id'])
-    info(u'Found %d documents project:%s space:%s', len(docs), project, space)
+    print(u'Found %d documents project:%s space:%s' % (len(docs), project, space))
     for doc in docs:
         print('  - ', doc.title)
 
@@ -125,8 +164,8 @@ def load_polarion(project, space):
             ('updated', doc.updated),
         ])
 
-        info(u'Reading (%2d/%2d) %-20s %-60s',
-             doc_idx + 1, len(docs), doc.type, doc.title)
+        print(u'Reading (%2d/%2d) %-20s %-60s' %
+             (doc_idx + 1, len(docs), doc.type, doc.title))
         fields = ['work_item_id', 'type', 'title', 'updated']
         wis = doc.get_work_items(None, True, fields=fields)
         for wi_idx, wi in enumerate(wis):
@@ -141,17 +180,33 @@ def load_polarion(project, space):
     return flatten_cases(obj)
 
 
-def flatten_cases(cases):
-    all_cases = {}
-    for doc_id, doc in cases['documents'].items():
-        wis = doc['work_items']
-        for wi_id, wi in wis.items():
-            if 'documents' in wi:
-                wi['documents'].append(doc_id)
-            else:
-                wi['documents'] = [doc_id]
-            all_cases[wi_id] = wi
-    return all_cases
+def check_and_load_automated(cases, linkage):
+    for case in linkage:
+        if 'automated' not in case:
+            case['automated'] = True
+        if 'polarion' not in case:
+            case['polarion'] = {}
+
+    # TODO: Check all manual cases. Not only automated cases.
+    session = _WorkItem.session
+    session.tx_begin()
+    for case in linkage:
+        if case['automated']:
+            wi_ids = case['polarion'].keys()
+            for wi_id in wi_ids:
+                automation = None
+                wi = _WorkItem(project_id=PROJECT, work_item_id=wi_id)
+                for custom in wi._suds_object.customFields.Custom:
+                    if custom.key == 'caseautomation':
+                        automation = custom.value.id
+
+                if automation in ['notautomated', None]:
+                    print("Setting %s" % wi_id)
+                    value = EnumOptionId(enum_id='automated')
+                    wi._set_custom_field('caseautomation', value._suds_object)
+                else:
+                    print("%s have automation state: %s" % (wi_id, automation))
+    session.tx_commit()
 
 
 def check_linkage(cases, linkage):
@@ -223,37 +278,12 @@ def check_linkage(cases, linkage):
     print('=' * 80)
 
 
-def check_update_automation(linkage):
-    for case in linkage:
-        if 'automated' not in case:
-            case['automated'] = True
-        if 'polarion' not in case:
-            case['polarion'] = {}
-
-    # TODO: Check all manual cases. Not only automated cases.
-    session = _WorkItem.session
-    session.tx_begin()
-    for case in linkage:
-        if case['automated']:
-            wi_ids = case['polarion'].keys()
-            for wi_id in wi_ids:
-                automation = None
-                wi = _WorkItem(project_id=PROJECT, work_item_id=wi_id)
-                for custom in wi._suds_object.customFields.Custom:
-                    if custom.key == 'caseautomation':
-                        automation = custom.value.id
-
-                if automation in ['notautomated', None]:
-                    print("Setting %s" % wi_id)
-                    value = EnumOptionId(enum_id='automated')
-                    wi._set_custom_field('caseautomation', value._suds_object)
-                else:
-                    print("%s have automation state: %s" % (wi_id, automation))
-    session.tx_commit()
-
-
-def merge_cases(base, current, updates, linkage):
-    polarion_dict = current
+def check_and_merge_cases(cases, linkage):
+    """
+    Merge workitem dict and linkage array into a list
+    Print all errors founded
+    """
+    polarion_dict = cases
 
     linkage_dict = {}
     for case in linkage:
@@ -262,39 +292,32 @@ def merge_cases(base, current, updates, linkage):
                 print('Multiple polarion for %s %s' %
                       (case['title'], case['polarion'].keys()))
             for wi_id in case['polarion']:
-                result_case = copy.deepcopy(case)
-                result_case['tcms'] = case.get('tcms', {}).keys()
-                result_case['automated'] = case.get('automated', True)
-                linkage_dict[wi_id] = result_case
+                if wi_id not in polarion_dict:
+                    print('Polarion workitem deleted' + str(wi_id))
+                    continue
+                if wi_id in linkage_dict:
+                    print('Merge cases list for' + str(wi_id))
+                    result_case = linkage_dict[wi_id]
+                    if case['feature'] != result_case['feature']:
+                        print('Merge failed' + str(wi_id))
+                    print(result_case.keys())
+                    print(case.keys())
+                    result_case['cases'] += case['cases']
+                    result_case['tcms'] += case.get('tcms', {}).keys()
+                else:
+                    result_case = copy.deepcopy(case)
+                    result_case['tcms'] = case.get('tcms', {}).keys()
+                    result_case['automated'] = case.get('automated', True)
+                    linkage_dict[wi_id] = result_case
         else:
             print('%s do not have polarion' % case['title'])
 
-    defaults = {
-        'title': '',
-        'polarion': '',
-        'tcms': '',
-        'feature': '',
-        'comment': '',
-        'automated': False,
-        'el6': True,
-        'el7': True,
-        'type': '',
-        'updated': None,
-        'created': False,
-        'removed': False,
-        'cases': [],
-        'documents': [],
-        'errors': [],
-        'changes': [],
-        'diffs': [],
-    }
-
+    defaults = ALL_CASE_DEFAULT_ATTR
     all_cases = []
-    polarion_ids = set(linkage_dict) | set(base) | set(current)
-    for wi_id in polarion_ids:
+    all_cases_ids = set(linkage_dict) | set(polarion_dict)
+    for wi_id in all_cases_ids:
         polarion_case = polarion_dict.get(wi_id, {})
         linkage_case = linkage_dict.get(wi_id, {})
-        updates_case = updates.get(wi_id, {})
 
         case = {}
         for key in defaults:
@@ -302,156 +325,14 @@ def merge_cases(base, current, updates, linkage):
                 case[key] = polarion_case.get(key)
             elif key in linkage_case:
                 case[key] = linkage_case.get(key)
-            elif key in updates_case:
-                case[key] = updates_case.get(key)
             else:
-                case[key] = defaults[key]
+                case[key] = defaults[key][:] if isinstance(defaults[key], list) else defaults[key]
         case['polarion'] = wi_id
         all_cases.append(case)
     return all_cases
 
 
-def recent_changes(wi_id, start):
-    uri = 'subterra:data-service:objects:/default/%s${WorkItem}%s' % (
-        PROJECT, wi_id)
-    changes = service.generateHistory(uri)
-    latest_changes = []
-    for change in changes:
-        if change.date > start:
-            latest_changes.append(suds_2_object(change))
-    return latest_changes
-
-
-def convert_text(text):
-    lines = re.split(r'\<[bB][rR]/\>', text)
-    new_lines = []
-    for line in lines:
-        line = " ".join(line.split())
-        line = HTMLParser.HTMLParser().unescape(line)
-        new_lines.append(line)
-    return '\n'.join(new_lines)
-
-
-def diff_test_steps(before, after):
-    steps_before = before['steps']['TestStep']
-    steps_after = after['steps']['TestStep']
-
-    if not isinstance(steps_before, list):
-        steps_before = [steps_before]
-    if not isinstance(steps_after, list):
-        steps_after = [steps_after]
-
-    diff_txt = ''
-    if len(steps_before) == len(steps_after):
-        for idx in range(len(steps_before)):
-            step_before, result_before = [
-                convert_text(text['content'])
-                for text in steps_before[idx]['values']['Text']]
-            step_after, result_after = [
-                convert_text(text['content'])
-                for text in steps_after[idx]['values']['Text']]
-            if step_before != step_after:
-                diff_txt += 'Step %s changed:\n' % (idx + 1)
-                for line in difflib.unified_diff(step_before.splitlines(1),
-                                                 step_after.splitlines(1)):
-                    diff_txt += line
-                diff_txt += '\n'
-            if result_before != result_after:
-                diff_txt += 'Result %s changed:\n' % (idx + 1)
-                for line in difflib.unified_diff(result_before.splitlines(1),
-                                                 result_after.splitlines(1)):
-                    diff_txt += line
-                diff_txt += '\n'
-    else:
-        diff_txt = ('Steps count changed %s --> %s' %
-                    (len(steps_before), len(steps_after)))
-    return diff_txt
-
-
-def check_changes(changes, wi_id=None):
-    diffs = []
-    for change in changes:
-        for diff in change['diffs']['item']:
-            field = diff['fieldName']
-            # Ignore irrelevant properties changing
-            if field in [
-                    'updated', 'outlineNumber', 'caseautomation', 'status',
-                    'previousStatus', 'approvals', 'tcmscaseid']:
-                continue
-
-            # Ignore parent item changing
-            if (field == 'linkedWorkItems' and
-                    diff['added'] and diff['removed'] and
-                    len(diff['added']['item']) == 1 and
-                    len(diff['removed']['item']) == 1 and
-                    diff['added']['item'][0]['role']['id'] == 'parent' and
-                    diff['removed']['item'][0]['role']['id'] == 'parent'):
-                continue
-
-            result_diff = {'field': field, 'added': [], 'removed': [],
-                           'changed': ''}
-
-            if diff['added']:
-                adds = diff['added']['item']
-                for add in adds:
-                    result_diff['added'].append(add)
-
-            if diff['removed']:
-                removes = diff['removed']['item']
-                for remove in removes:
-                    result_diff['removed'].append(remove)
-
-            if 'before' in diff or 'after' in diff:
-                before = diff.get('before', '')
-                after = diff.get('after', '')
-
-                if field == 'testSteps':
-                    result_diff['changed'] = diff_test_steps(before, after)
-                    diffs.append(result_diff)
-                    continue
-
-                if 'id' in before:
-                    before = before['id']
-                if 'id' in after:
-                    after = after['id']
-                if 'content' in before:
-                    before = convert_text(before['content'])
-                if 'content' in after:
-                    after = convert_text(after['content'])
-                before += '\n'
-                after += '\n'
-
-                diff_txt = ''
-                for line in difflib.unified_diff(
-                        before.splitlines(1),
-                        after.splitlines(1)):
-                    diff_txt += line
-                result_diff['changed'] = diff_txt
-                diffs.append(result_diff)
-    return diffs
-
-
-def load():
-    # Load base status recorded
-    with open('base_polarion.yaml') as base_fp:
-        base = yaml.load(base_fp)
-
-    # Load current polarion status
-    # current = load_polarion(project, MANUAL_SPACE)
-    with open('current_polarion.yaml') as current_fp:
-        current = yaml.load(current_fp)
-
-    # Load linkage configured
-    with open('autotest_cases.yaml') as linkage_fp:
-        linkage = yaml.load(linkage_fp)
-
-    with open('updates.yaml') as updates_fp:
-        updates = yaml.load(updates_fp)
-
-    return merge_cases(base, current, updates, linkage)
-
-
-def check(cases):
+def check_cases_list(cases):
     """
     Check all the data collected and mark corresponding error message if
     error exists.
@@ -487,40 +368,53 @@ def check(cases):
         if case['title'] in duplicates:
             case['errors'].append('Duplicate name found')
 
-        if case['changes']:
-            case['diffs'] = check_changes(case['changes'], wi_id)
-            if case['diffs'] and case['automated']:
-                case['errors'].append('Updates not confirmed')
-            else:
-                case['accept_updates'] = True
-
 
 def update_base(cases):
     new_base = {}
+    defaults = BASELINE_WORKITEM_ATTR
     for case in cases:
         new_case = {}
-        wi_id = case['polarion']
-        new_case['documents']
+        for key in defaults:
+            if key in case:
+                new_case[key] = case.get(key)
+            else:
+                new_case[key] = defaults[key][:] if isinstance(defaults[key], list) else defaults[key]
 
+        wi_id = case['polarion']
         new_base[wi_id] = new_case
+
     with open('base_polarion_new.yaml', 'w') as base_fp:
         yaml.dump(new_base, base_fp)
+
+
+def load():
+    # Load current polarion status
+    #current = load_polarion(PROJECT, MANUAL_SPACE)
+    #with open('current_polarion.yaml') as polarion_fp:
+    #    yaml.dump(current, polarion_fp)
+
+    with open('current_polarion.yaml') as polarion_fp:
+        current = yaml.load(polarion_fp)
+
+    # Load linkage configured
+    with open('autotest_cases.yaml') as linkage_fp:
+        linkage = yaml.load(linkage_fp)
+
+    #check_and_load_automated(current, linkage)
+    #check_linkage(current, linkage)
+
+    case_list = check_and_merge_cases(current, linkage)
+
+    check_cases_list(case_list)
+    return case_list
 
 
 def run():
     # Load stage
     all_cases = load()
 
-    # Check stage
-    check(all_cases)
-#    linkage_result = check_linkage(current, linkage)
-
-    # Update stage
-    # check_updata_automation(linkage)
+    #dump_workitem(all_cases)
     update_base(all_cases)
-
-    # Report stage
-    # report(polarion_result)
 
 
 if __name__ == '__main__':
