ea7be1375b34fb4b4eb73470a670ad13a0b4220a
Kairui Song <kasong@redhat.com>
2016-06-22 13:57:52 +0800

    New db model

--- caselink/models.py
+++ caselink/models.py
@@ -2,68 +2,75 @@ from django.db import models
 
 
 class Error(models.Model):
+    id = models.CharField(max_length=255, primary_key=True)
+    message = models.CharField(max_length=255, blank=True)
+    def __str__(self):
+        return self.id + ":" + self.message
+
+
+class Arch(models.Model):
+    name = models.CharField(max_length=255, primary_key=True)
+    def __str__(self):
+        return self.name
+
+
+class Component(models.Model):
+    name = models.CharField(max_length=255, primary_key=True)
+    def __str__(self):
+        return self.name
+
+
+class Framework(models.Model):
+    name = models.CharField(max_length=255, primary_key=True)
+    components = models.ManyToManyField(Component)
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    id = models.CharField(max_length=255, primary_key=True)
     name = models.CharField(max_length=255)
     def __str__(self):
         return self.name
 
 
-class WorkItem(models.Model):
-    wi_id = models.CharField(max_length=255, primary_key=True)
-    title = models.CharField(
-        max_length=65535,
-        blank=True,
-    )
-    type = models.CharField(
-        max_length=255,
-        blank=True,
-    )
-    automation = models.CharField(
-        max_length=255,
-        default='notautomated',
-        blank=True,
-    )
-    feature = models.CharField(
-        max_length=255,
-        default='',
-        blank=True,
-    )
-    comment = models.CharField(
-        max_length=65535,
-        default='',
-        blank=True,
-    )
-    confirmed = models.DateTimeField(
-        blank=True,
-        null=True,
-    )
-    updated = models.DateTimeField(
-        blank=True,
-        null=True,
-    )
-    errors = models.ManyToManyField(Error)
-
-    def __str__(self):
-        return self.wi_id
-
-
-
 class Document(models.Model):
-    doc_id = models.CharField(max_length=65535)
-    workitems = models.ManyToManyField(WorkItem)
+    id = models.CharField(max_length=255, primary_key=True)
+    components = models.ManyToManyField(Component)
+    title = models.CharField(max_length=65535)
+    def __str__(self):
+        return self.id
 
 
-class Change(models.Model):
-    workitem = models.ForeignKey(WorkItem)
-    revision = models.IntegerField()
-    obj = models.CharField(max_length=65535)
-    diff = models.CharField(max_length=65535)
+class WorkItem(models.Model):
+    id = models.CharField(max_length=255, primary_key=True)
+    type = models.CharField(max_length=255, blank=True)
+    title = models.CharField(max_length=65535, blank=True)
+    automation = models.CharField(max_length=255, default='notautomated', blank=True)
+    commit = models.CharField(max_length=255, blank=True)
+    project = models.ForeignKey(Project, blank=True, null=True, related_name='workitems')
+    archs = models.ManyToManyField(Arch, blank=True, related_name='workitems')
+    documents = models.ManyToManyField(Document, blank=True, related_name='workitems')
+    errors = models.ManyToManyField(Error, blank=True, related_name='workitems')
+    def __str__(self):
+        return self.id
 
 
-class AvocadoCase(models.Model):
-    workitems = models.ManyToManyField(WorkItem)
-    name = models.CharField(max_length=65535, primary_key = True)
+class AutoCase(models.Model):
+    id = models.CharField(max_length=65535, primary_key=True)
+    archs = models.ManyToManyField(Arch, blank=True, related_name='autocases')
+    framework = models.ForeignKey(Framework, null=True, on_delete=models.PROTECT,
+                                  related_name='autocases')
+    start_commit = models.CharField(max_length=255, blank=True)
+    end_commit = models.CharField(max_length=255, blank=True)
+    errors = models.ManyToManyField(Error, blank=True, related_name='autocases')
 
 
-class TCMSCase(models.Model):
-    workitems = models.ManyToManyField(WorkItem)
-    tcmsid = models.CharField(max_length=255)
+class CaseLink(models.Model):
+    workitem = models.ForeignKey(WorkItem, on_delete=models.PROTECT, related_name='caselinks')
+    autocases = models.ManyToManyField(AutoCase, blank=True, related_name='caselinks')
+    autocase_pattern = models.CharField(max_length=255, blank=True)
+    framework = models.ForeignKey(Framework, on_delete=models.PROTECT, null=True,
+                                  related_name='caselinks')
+    errors = models.ManyToManyField(Error, blank=True, related_name='caselinks')
+
