61d34f13c93191fc2c2f88b9d02fada7a5a55d7d
Kairui Song <kasong@redhat.com>
2016-09-26 19:05:33 +0800

    Items shown in statistic graph can be seleted.

--- static/javascript/test-run-stack-graph.js
+++ static/javascript/test-run-stack-graph.js
@@ -6,40 +6,36 @@
         width = 960 - margin.left - margin.right,
         height = 500 - margin.top - margin.bottom;
 
-    var TestRunStackGraph = function(dom_selector, testRun){
-        this.layer_keys = ["auto_passed", "auto_failed", "auto_skipped", "auto_error"];
-        this.layer_colors = ["limegreen", "tomato", "darkorange", "lightgray"];
-        this.n = this.layer_keys.length;
+    var keyword_colors = {
+        "auto_passed": "limegreen",
+        "auto_failed": "tomato",
+        "auto_skipped": "darkorange",
+        "auto_error": "lightgray"
+    };
 
+    var TestRunStackGraph = function(dom_selector, testRun){
+        this.layer_keys = Object.keys(keyword_colors);
+        this.n = this.layer_keys.length;
         this.stack = d3.stack()
             .keys(this.layer_keys)
             .order(d3.stackOrderNone)
             .offset(d3.stackOffsetNone);
 
-        this.transitionGrouped = function(){};
-        this.transitionStacked = function(){};
-        this.timeout = null;
         this.initGraph(dom_selector);
-        this.updateData(testRun);
-        this.presentation = "stacked";
+        var that = this;
+        this.updateData(testRun).then(function(){
+            that.render();
+        });
     };
 
     TestRunStackGraph.prototype.changeKeys = function(keys){
-        this.layer_keys = ["auto_passed", "auto_failed", "auto_skipped", "auto_error"];
+        this.layer_keys = keys;
         this.n = this.layer_keys.length;
         this.stack.keys(this.layer_keys);
-    };
-
-    TestRunStackGraph.prototype.changePresentation = function(value){
-        this.presentation = value;
-        if (this.timeout !== null){
-            clearTimeout(this.timeout);
-            this.timeout = null;
+        if(this.data){
+            //If data is undefined, graph is still initializing.
+            this.render();
         }
-        if (value === "grouped")
-            this.transitionGrouped();
-        else
-            this.transitionStacked();
     };
 
     TestRunStackGraph.prototype.initGraph = function(dom_selector){
@@ -68,101 +64,82 @@
             .attr("transform", "translate(0," + height + ")");
     };
 
-    TestRunStackGraph.prototype.updateData = function(testRun){
+    TestRunStackGraph.prototype.render = function(){
+        var n = this.n,// number of layers
+            m = this.data.length, // number of samples per layer
+            layers = this.stack(this.data),
+            yStackMax = d3.max(layers, function(layer) { return d3.max(layer, function(d) { return d[1]; }); });
+
         var x = this.x,
             y = this.y,
             xAxis = this.xAxis,
             svg = this.svg;
 
         var that = this;
+
+        //Update scale, layer etc.
+        this.x.domain(d3.range(m));
+        this.y.domain([yStackMax, 0]);
+
+        xAxis.scale(x);
+
+        var layer = this.svg.selectAll(".layer")
+            .data(layers);
+
+        svg.selectAll("g.xAxis").transition().call(xAxis);
+
+        //perform redraw and transition
+        layer.enter().append("g")
+            .attr("class", "layer")
+            .merge(layer)
+            .style("fill", function(d, i) { return keyword_colors[that.layer_keys[i]] || 'red'; });
+
+        layer.exit().remove();
+
+        layer = this.svg.selectAll(".layer");
+
+        var rect = layer.selectAll("rect")
+            .data(function(d) { return d; });
+
+        rect.exit()
+            .transition()
+            .attr("y", height)
+            .attr("height", 0)
+            .remove();
+
+        rect = rect.enter().append("rect")
+            //Set start point for new added rect
+            .attr("x", function(d, i) { return x(i); })
+            .attr("y", height)
+            .attr("width", x.bandwidth())
+            .attr("height", 0)
+            //Merge new added(enter) and existing(update)
+            .merge(rect);
+
+        rect.transition()
+            //Transform to zero
+            .attr("y", height)
+            .attr("x", function(d, i) { return x(i); })
+            .attr("width", x.bandwidth())
+            .attr("height", 0)
+            //Transform to the right positoin
+            .transition()
+            .delay(function(d, i) { return i * 10; })
+            .attr("y", function(d) { return height - y(d[0]) - (y(d[1]) - y(d[0])); })
+            .attr("height", function(d, i) { return y(d[1]) - y(d[0]); });
+    };
+
+    TestRunStackGraph.prototype.updateData = function(testRun){
+        var that = this;
         // TODO: hard coded url
-        $.get('/statistics/run/' + (testRun || ''), function(data){
+        return $.get('/statistics/run/' + (testRun || ''), function(data){
             results = [];
             for (var key in data){
                 results = results.concat(data[key]);
             }
 
-            var n = this.n,// number of layers
-                m = results.length, // number of samples per layer
-                layers = that.stack(results),
-                yGroupMax = d3.max(layers, function(layer) { return d3.max(layer, function(d) { return d[0]; }); }),
-                yStackMax = d3.max(layers, function(layer) { return d3.max(layer, function(d) { return d[0] + d[1]; }); });
-
-            that.x.domain(d3.range(m));
-
-            that.y.domain([0, yStackMax]);
-
-            xAxis.scale(x);
-
-            svg.selectAll("g.xAxis").transition().call(xAxis);
-
-            //Update data for each layer
-            var layer = that.svg.selectAll(".layer")
-                .data(layers);
-
-            layer.enter().append("g")
-                .attr("class", "layer")
-                .style("fill", function(d, i) { return that.layer_colors[i] | 'red'; });
-
-            layer = that.svg.selectAll(".layer");
-
-            var rect = layer.selectAll("rect")
-                .data(function(d) { return d; });
-
-            rect.exit()
-                .transition()
-                .attr("y", height)
-                .attr("height", 0)
-                .remove();
-
-            rect = rect.enter().append("rect")
-                //Set start point for new added rect
-                .attr("x", function(d, i) { return x(i); })
-                .attr("y", height)
-                .attr("width", x.bandwidth())
-                .attr("height", 0)
-                //Merge new added(enter) and existing(update)
-                .merge(rect);
-
-            rect.transition()
-                //Transform to zero
-                .attr("y", height)
-                .attr("x", function(d, i) { return x(i); })
-                .attr("width", x.bandwidth())
-                .attr("height", 0)
-                //Transform to the right positoin
-                .transition()
-                .delay(function(d, i) { return i * 10; })
-                .attr("y", function(d) { return y(d[0] + d[1]); })
-                .attr("height", function(d) {return y(d[0]) - y(d[0] + d[1]); });
-
-            that.transitionGrouped = function() {
-                y.domain([0, yGroupMax]);
-
-                rect.transition()
-                    .duration(500)
-                    .delay(function(d, i) { return i * 10; })
-                    //Ouch!
-                    .attr("x", function(d, i) {return x(i) + x.bandwidth() / n * (Array.prototype.indexOf.call(this.parentNode.parentNode.childNodes, this.parentNode)); })
-                    .attr("width", x.bandwidth() / n)
-                .transition()
-                    .attr("y", function(d) { return y(d[1]); })
-                    .attr("height", function(d) { return height - y(d[1]); });
-            };
-
-            that.transitionStacked = function() {
-                y.domain([0, yStackMax]);
-
-                rect.transition()
-                    .duration(500)
-                    .delay(function(d, i) { return i * 10; })
-                    .attr("y", function(d) { return y(d[0] + d[1]); })
-                    .attr("height", function(d) { return y(d[0]) - y(d[0] + d[1]); })
-                .transition()
-                    .attr("x", function(d, i) { return x(i); })
-                    .attr("width", x.bandwidth());
-            };
-        });
+            that.data = results;
+        }).promise();
     };
 
     root.TestRunStackGraph = TestRunStackGraph;
