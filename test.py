1b236e4490d548fb02bda753aa60dcffaa069178
ryncsn <ryncsn@gmail.com>
2016-10-05 18:13:47 +0800

    Use ./test.py --fixfure to generate some fixture for dev.

--- test.py
+++ test.py
@@ -4,20 +4,39 @@ import os
 import app
 import unittest
 import tempfile
+import argparse
 
 from flask import json, jsonify
-
 
 class DashboardTestCase(unittest.TestCase):
     keep_data = False
     def setUp(self):
+        if self.keep_data:
+            return self.setUpDev()
+        else:
+            return self.setUpTest()
+
+    def tearDown(self):
+        if self.keep_data:
+            return self.tearDownDev()
+        else:
+            return self.tearDownTest()
+
+    def setUpDev(self):
+        self.app = app.app.test_client()
+        app.init_db()
+
+    def tearDownDev(self):
+        pass
+
+    def setUpTest(self):
         (self.db_fd, self.db_fn) = tempfile.mkstemp()
         app.app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + self.db_fn
         app.app.config['TESTING'] = True
         self.app = app.app.test_client()
         app.init_db()
 
-    def tearDown(self):
+    def tearDownTest(self):
         os.close(self.db_fd)
         os.unlink(self.db_fn)
 
@@ -28,8 +47,7 @@ class EmptyDBTest(DashboardTestCase):
         assert b'[]' in rv.data
 
 
-class TestResultSubmitTest(DashboardTestCase):
-    #TODO: Keep data
+class FixtureTest(DashboardTestCase):
     def submit_test_run(self):
         post_data = {
             "name": "libvirt-RHEL-7.3-runtest-x86_64-acceptance-general",
@@ -68,7 +86,6 @@ class TestResultSubmitTest(DashboardTestCase):
             #TODO: Number / String
             assert str(post_data[key]) == str(rv_data[key])
 
-
     def submit_auto_failed_result(self, name, failure):
         post_data = {
             "output": "OUTPUT CONTENT",
@@ -82,8 +99,7 @@ class TestResultSubmitTest(DashboardTestCase):
             assert key in rv_data
             assert str(post_data[key]) == str(rv_data[key])
 
-
-    def test_run(self):
+    def test_submit_data(self):
         self.submit_test_run()
         for case in ["a.b.c.d",
                      "a.b.c.4.e",
@@ -109,7 +125,23 @@ class TestResultSubmitTest(DashboardTestCase):
             ("Failure UnKnown", "1.4.1")]:
             self.submit_auto_failed_result(case, failure)
 
+    def runTest(self):
+        return self.test_submit_data()
+
+
+def run():
+    parser = argparse.ArgumentParser(description='Unit tests and fixtures for libvirt-dashboard.')
+    parser.add_argument('--fixture', dest='fixture', action='store_true',
+                        default=False, help=('Generate fixture for test/development then exit.'))
+    args = parser.parse_args()
+
+    if args.fixture:
+        DashboardTestCase.keep_data = True
+        test = FixtureTest()
+        test.debug()
+    else:
+        unittest.main()
+    sys.exit(0)
 
 if __name__ == '__main__':
-    unittest.main()
-    sys.exit(0)
+    run()
