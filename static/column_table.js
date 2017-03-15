0c818355c6eb3918c248220ec95b070c57cc91a0
Kairui Song <kasong@redhat.com>
2017-03-15 10:23:50 +0800

    Simplify column table code.

--- static/column_table.js
+++ static/column_table.js
@@ -1,22 +1,15 @@
-var dtMixins = require('datatables-mixins');
+/* A simple datatable for presenting any db */
 var htmlify = require('./lib/htmlify.js');
 var _p = require("./lib/sharedParameters.js");
-var columns = [];
+var columns = _p.get("templateColumns").map(col => ({
+  "render": htmlify,
+  "data": column
+}));
 
-for (var columnSrc of _p.get("templateColumns")){
-  columns.push({
-    "render": htmlify,
-    "data": columnSrc
-  });
-}
-
-$(document).ready(function() {
-  $('#column_table').dataTable( {
-    pageLength: 50,
-    columns: columns,
-    ajax: {
-      url: _p.get("ajaxURL"),
-      dataSrc: ''
-    },
-  });
+$('#column_table').dataTable( {
+  columns: columns,
+  ajax: {
+    url: _p.get("ajaxURL"),
+    dataSrc: ''
+  },
 });
