3e63bc4cec6f37bb1a6a4439e72d7c71c0ee2021
Kairui Song <kasong@redhat.com>
2017-05-02 15:48:57 +0800

    Add new migrate file and make migration work again

--- migrations/env.py
+++ migrations/env.py
@@ -2,7 +2,7 @@ from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
-from model import db
+from app.model import db
 import logging
 
 # this is the Alembic Config object, which provides
