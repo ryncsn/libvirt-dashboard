f7d4e8d629873201a723703b22bf1a53c36bb041
Kairui Song <kasong@redhat.com>
2017-05-02 18:09:42 +0800

    Generate and submit XML instead of using WSDL
    Will submit the XML later

--- config/__init__.py
+++ config/__init__.py
@@ -10,12 +10,15 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     POLARION_ENABLED = False
-    POLARION_URL = '#'
-    POLARION_PLAN = {
+    POLARION_URL = 'https://localhost/'
+    POLARION_PROJECT = 'TEST-PROJECT'
+    POLARION_USER = 'TEST-USER'
+    POLARION_PASSWORD = 'TEST-PASSWORD'
+    POLARION_PLANS = {
         "PRODUCT-VERSION": "QUERY",
     }
 
-    CELERY_BROKER='amqp://guest:guest@localhost:5672//'
+    CELERY_BROKER = 'amqp://guest:guest@localhost:5672//'
     CELERY_RESULT_BACKEND = 'db+sqlite:////tmp/test.db'
     CELERY_IGNORE_RESULT = False
 
